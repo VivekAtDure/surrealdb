74e231e9245481a12d677d98219a6fb978ecc255
Vivek <vivek.gupta@duretechnologies.com>
2026-04-24 17:49:13 +0530

    remove infer_sentiment

--- start.bat
+++ start.bat
@@ -3,4 +3,4 @@ echo Starting SurrealDB v3.0.5 with RocksDB (persistent file-based storage)...
 echo Listening on http://localhost:8000
 echo Username: root / Password: root
 echo.
-surreal start --user root --pass root --bind 0.0.0.0:8000 --allow-net=llm.imonitorplus.com "rocksdb:///d:/LLM-repository/POC/surrealdb/data-rocksdb"
+surreal start --user root --pass root --bind 0.0.0.0:8000 "rocksdb:///d:/LLM-repository/POC/surrealdb/data-rocksdb"
